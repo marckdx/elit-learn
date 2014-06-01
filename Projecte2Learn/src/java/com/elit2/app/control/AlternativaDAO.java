315fba449eb164e31cf583082463e719d5604bf3
Marco Aurélio <Marco Aurlio@MARCO-NOTE>
2014-06-01 11:28:57 -0300

    Adicoinados métodos de update e delete em Alternativa.

--- Projecte2Learn/src/java/com/elit2/app/control/AlternativaDAO.java
+++ Projecte2Learn/src/java/com/elit2/app/control/AlternativaDAO.java
@@ -23,25 +23,39 @@ public class AlternativaDAO {
     ResultSet rs;
 
     public ArrayList<Alternativa> getAlternativas(Questao questao) throws Exception {
-        String sql = "SELECT * FROM tb_altern WHERE tb_quest_cd_quest="+questao.getCd_questao();
+        String sql = "SELECT * FROM tb_altern WHERE tb_quest_cd_quest=" + questao.getCd_questao();
         con = new OracleConnector().getConnection();
         stmt = con.createStatement();
         rs = stmt.executeQuery(sql);
         ArrayList<Alternativa> alters = new ArrayList<Alternativa>();
         while (rs.next()) {
-           Alternativa alter = new Alternativa(rs.getInt("cd_alternativa"), rs.getString("ds_alternativa"));
-           alters.add(alter);
+            Alternativa alter = new Alternativa(rs.getInt("cd_alternativa"), rs.getString("ds_alternativa"));
+            alters.add(alter);
         }
         con.close();
         stmt.close();
         rs.close();
         return alters;
     }
-    
-      public int setAlternativa(Questao questao, Alternativa alter) throws Exception{
+
+    public int setAlternativa(Questao questao, Alternativa alter) throws Exception {
         con = new OracleConnector().getConnection();
         stmt = con.createStatement();
-        int result = stmt.executeUpdate("INSERT INTO tb_altern VALUES ("+alter.getCd_alternativa()+",'"+alter.getDs_alternativa()+"',"+questao.getCd_questao()+")");
+        int result = stmt.executeUpdate("INSERT INTO tb_altern VALUES (" + alter.getCd_alternativa() + ",'" + alter.getDs_alternativa() + "'," + questao.getCd_questao() + ")");
+        return result;
+    }
+
+    public int updAlternativa(Alternativa alter) throws Exception {
+        con = new OracleConnector().getConnection();
+        stmt = con.createStatement();
+        int result = stmt.executeUpdate("UPDATE tb_altern SET ds_altern='" + alter.getDs_alternativa() + "' WHERE cd_altern=" + alter.getCd_alternativa());
+        return result;
+    }
+
+    public int delAlternativa(Alternativa alter) throws Exception {
+        con = new OracleConnector().getConnection();
+        stmt = con.createStatement();
+        int result = stmt.executeUpdate("DELETE FROM tb_altern WHERE cd_altern=" + alter.getCd_alternativa());
         return result;
     }
 }
