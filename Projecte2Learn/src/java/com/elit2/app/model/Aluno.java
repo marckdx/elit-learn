29e828d30c4d479b5314ae513f8d8d6b17083d30
Jhonatan Rodrigues Monteiro <jhonatan.rodrigues@live.com>
2014-06-01 02:07:41 -0300

    Model aluno renomeado

--- Projecte2Learn/src/java/com/elit2/app/model/Aluno.java
+++ Projecte2Learn/src/java/com/elit2/app/model/Aluno.java
@@ -11,30 +11,30 @@ package com.elit2.app.model;
  * @author Marco Aurélio
  */
 public class Aluno extends Usuario{
-    private int cd_aluno;
-    private String nm_aluno;
+    private int cd_alu;
+    private String nm_alu;
     private int cd_nivel;
 
-    public Aluno(int cd_aluno, String nm_aluno, int cd_nivel) {
-        this.cd_aluno = cd_aluno;
-        this.nm_aluno = nm_aluno;
+    public Aluno(int cd_alu, String nm_alu, int cd_nivel) {
+        this.cd_alu = cd_alu;
+        this.nm_alu = nm_alu;
         this.cd_nivel = cd_nivel;
     }
 
     public int getCd_aluno() {
-        return cd_aluno;
+        return cd_alu;
     }
 
-    public void setCd_aluno(int cd_aluno) {
-        this.cd_aluno = cd_aluno;
+    public void setCd_aluno(int cd_alu) {
+        this.cd_alu = cd_alu;
     }
 
     public String getNm_aluno() {
-        return nm_aluno;
+        return nm_alu;
     }
 
-    public void setNm_aluno(String nm_aluno) {
-        this.nm_aluno = nm_aluno;
+    public void setNm_aluno(String nm_alu) {
+        this.nm_alu = nm_alu;
     }
 
     public int getCd_nivel() {
