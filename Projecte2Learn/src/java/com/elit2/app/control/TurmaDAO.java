4b3f714e666146756d603fd5fdf8980f33127e09
Jhonatan Rodrigues Monteiro <jhonatan.rodrigues@live.com>
2014-06-01 11:54:42 -0300

    Implementado TurmaDAO

--- Projecte2Learn/src/java/com/elit2/app/control/TurmaDAO.java
+++ Projecte2Learn/src/java/com/elit2/app/control/TurmaDAO.java
@@ -3,13 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.elit2.app.control;
 
-/**
- *
- * @author Jhonatan
- */
+import com.elit2.app.model.Turma;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class TurmaDAO {
-    
+
+    Connection con;
+    Statement stmt;
+    ResultSet rs;
+
+    public ArrayList<Turma> getTurma(Turma tur) throws Exception {
+
+        String sql = "SELECT * FROM tb_tur";
+        con = new OracleConnector().getConnection();
+        stmt = con.createStatement();
+        rs = stmt.executeQuery(sql);
+        ArrayList<Turma> turmas = new ArrayList<Turma>();
+        while (rs.next()) {
+            Turma quest = new Turma(rs.getInt("cd_tur"), rs.getString("nm_tur"), rs.getInt("tb_curso_cd_curso"));
+            turmas.add(quest);
+        }
+        con.close();
+        stmt.close();
+        rs.close();
+        return turmas;
+    }
 }
